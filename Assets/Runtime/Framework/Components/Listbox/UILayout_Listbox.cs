67f384036f6f321ba7a26771836adf52e9f33b3d
Jérôme CREMOUX <jcremoux@gmail.com>
2023-01-27 15:45:57 +0100

    End of listbox implementation (custom listbox / comments, clean code, listbox component for grid)

--- Assets/Runtime/Framework/Components/Listbox/UILayout_Listbox.cs
+++ Assets/Runtime/Framework/Components/Listbox/UILayout_Listbox.cs
@@ -9,21 +9,49 @@ namespace Fugui.Framework
     public partial class UILayout
     {
         #region Generic Types List
+        /// <summary>
+        /// This method creates a Listbox with the given text and items.
+        /// </summary>
+        /// <param name="text">The text to be displayed above the listbox.</param>
+        /// <param name="items">The list of items to be displayed in the listbox.</param>
         public void Listbox<T>(string text, List<T> items)
         {
             Listbox<T>(text, items, null, null, UIListboxStyle.Default);
         }
 
+        /// <summary>
+        /// This method creates a Listbox with the given text and items, with specific height, and allows for actions to be performed when an item is selected and when the current item is retrieved.
+        /// </summary>
+        /// <param name="text">The text to be displayed above the listbox.</param>
+        /// <param name="items">The list of items to be displayed in the listbox.</param>
+        /// <param name="itemChange">The action to be performed when an item is selected.</param>
+        /// <param name="height">The height of the listbox component.</param>
+        /// <param name="itemGetter">A function that returns the current item.</param>
         public void Listbox<T>(string text, List<T> items, Action<T> itemChange, int height, Func<T> itemGetter = null)
         {
             Listbox<T>(text, items, itemChange, itemGetter, UIListboxStyle.Default);
         }
 
+        /// <summary>
+        /// This method creates a Listbox with the given text and items, and allows for actions to be performed when an item is selected and when the current item is retrieved
+        /// </summary>
+        /// <param name="text">The text to be displayed above the listbox.</param>
+        /// <param name="items">The list of items to be displayed in the listbox.</param>
+        /// <param name="itemChange">The action to be performed when an item is selected.</param>
+        /// <param name="itemGetter">A function that returns the current item.</param>
         public void Listbox<T>(string text, List<T> items, Action<T> itemChange, Func<T> itemGetter = null)
         {
             Listbox<T>(text, items, itemChange, itemGetter, UIListboxStyle.Default);
         }
 
+        /// <summary>
+        /// This method creates a Listbox with the given text and items, and allows for actions to be performed when an item is selected and when the current item is retrieved, with a specified style.
+        /// </summary>
+        /// <param name="text">The text to be displayed above the listbox.</param>
+        /// <param name="items">The list of items to be displayed in the listbox.</param>
+        /// <param name="itemChange">The action to be performed when an item is selected.</param>
+        /// <param name="itemGetter">A function that returns the current item.</param>
+        /// <param name="style">The style of the listbox.</param>
         public void Listbox<T>(string text, List<T> items, Action<T> itemChange, Func<T> itemGetter, UIListboxStyle style)
         {
             List<IListboxItem> cItems = new List<IListboxItem>();
@@ -47,6 +75,14 @@ namespace Fugui.Framework
         #endregion
 
         #region IListboxItems
+        /// <summary>
+        /// This method creates a custom listbox with the given parameters.
+        /// </summary>
+        /// <param name="text">The label text for the listbox.</param>
+        /// <param name="items">A list of items to be displayed in the listbox.</param>
+        /// <param name="itemChange">An action that is invoked when the selected item in the listbox changes.</param>
+        /// <param name="itemGetter">A function that returns the currently selected item in the listbox as a string.</param>
+        /// <param name="style">The style for the listbox.</param>
         protected virtual void _customListbox(string text, List<IListboxItem> items, Action<int> itemChange, Func<string> itemGetter, UIListboxStyle style)
         {
             text = beginElement(text, style);
@@ -127,6 +163,92 @@ namespace Fugui.Framework
             }
             FuGui.PopStyle();
             displayToolTip();
+            endElement(style);
+        }
+        #endregion
+
+        #region Fully custom listbox content
+        /// <summary>
+        /// Displays a listbox that allows the user to choose from a list of predefined items. 
+        /// When an item is selected, the specified callback function is called.
+        /// </summary>
+        /// <param name="text">The label displayed next to the listbox</param>
+        /// <param name="selectedItemText">The currently selected item</param>
+        /// <param name="callback">The callback function that is called when an item is selected</param>
+        /// <param name="height">The height of the list of items</param>
+        public void Listbox(string text, string selectedItemText, Action callback, int height = 0)
+        {
+            Listbox(text, selectedItemText, callback, UIListboxStyle.Default, height);
+        }
+
+        /// <summary>
+        /// Displays a listbox that allows the user to choose from a list of predefined items. 
+        /// When an item is selected, the specified callback function is called.
+        /// </summary>
+        /// <param name="text">The label displayed next to the listbox</param>
+        /// <param name="selectedItemText">The currently selected item</param>
+        /// <param name="callback">The callback function that is called when an item is selected</param>
+        /// <param name="style">The style of the listbox</param>
+        /// <param name="height">The height of the list of items</param>
+        public virtual void Listbox(string text, string selectedItemText, Action callback, UIListboxStyle style, int height = 0)
+        {
+            text = beginElement(text, style);
+
+            // Adjust the padding for the frame and window
+            FuGui.Push(ImGuiStyleVar.FramePadding, new Vector2(8f, 2f));
+            FuGui.Push(ImGuiStyleVar.WindowPadding, new Vector2(8f, 8f));
+
+            // Begin the listbox
+            if (ImGui.BeginListBox(text))
+            {
+                // Pop the padding styles
+                FuGui.PopStyle();
+                IsInsidePopUp = true;
+                // Check if a height has been specified
+                if (height > 0)
+                {
+                    // Invoke the callback with a fixed height for the combobox
+                    callback?.Invoke();
+                }
+                else
+                {
+                    // Invoke the callback without a fixed height for the combobox
+                    callback?.Invoke();
+                }
+                // Set the IsInsidePopUp flag to false
+                IsInsidePopUp = false;
+
+                // Check if the CurrentPopUpID is not equal to the given text
+                if (CurrentPopUpID != text)
+                {
+                    // Set the CurrentPopUpWindowID to the current drawing window ID
+                    CurrentPopUpWindowID = UIWindow.CurrentDrawingWindow?.ID;
+                    // Set the CurrentPopUpID to the given text
+                    CurrentPopUpID = text;
+                }
+                // Set CurrentPopUpRect to ImGui item rect
+                CurrentPopUpRect = new Rect(ImGui.GetWindowPos(), ImGui.GetWindowSize());
+                // End the combobox
+                ImGui.EndListBox();
+            }
+            else
+            {
+                // Pop the padding styles
+                FuGui.PopStyle();
+                // Check if the CurrentPopUpID is equal to the given text
+                if (CurrentPopUpID == text)
+                {
+                    // Set the CurrentPopUpWindowID to null
+                    CurrentPopUpWindowID = null;
+                    // Set the CurrentPopUpID to null
+                    CurrentPopUpID = null;
+                }
+            }
+            // Pop the padding styles
+            FuGui.PopStyle();
+            // Display the tooltip
+            displayToolTip();
+            // End the element with the current combobox style
             endElement(style);
         }
         #endregion

6bbbd8038bc307a040cc50197c2bf96ae7c528da
Keksls <vrdtmstudio@gmail.com>
2023-04-02 22:05:06 +0200

    merge Flags and adding AI training prompt + dataset

--- Runtime/Core/Flags.cs
+++ Runtime/Core/Flags.cs
@@ -1,6 +1,21 @@
 ﻿namespace Fu.Core
 {
     /// <summary>
+    /// Enum that represent an UI window state
+    /// </summary>
+    public enum FuWindowState
+    {
+        /// <summary>
+        /// UI is not focused and can be drawed few times per seconds
+        /// </summary>
+        Idle,
+        /// <summary>
+        /// UI is focused or manipulated and must be drawed many times per seconds
+        /// </summary>
+        Manipulating
+    }
+
+    /// <summary>
     /// Define Fugui font types
     /// </summary>
     public enum FontType
